7ab0e63bd0e05b977ee1e09b7ae3657dd5283d97
Jairo Quispe <jairo.quispe.03@gmail.com>
2018-11-25 21:41:48 -0500

    update

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let menuscroll = () => {
+    console.log("pepe");
+    let $navmenu = document.getElementById('nav-menu');
+    let $window = document.documentElement;
+    if ($window.scrollTop > 50) {
+        $navmenu.classList.add("is-scrolling");
+    } else {
+        $navmenu.classList.remove("is-scrolling");
+    }
+}
+;
+(()=>{
+
+})();
